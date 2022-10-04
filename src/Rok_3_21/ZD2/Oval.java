884e99d89f24b5e2e64ba33938560404df0163e2
lnenadic <lnenadic@student.unizd.hr>
2022-10-04 11:55:45 +0200

    added rok321_zd2

---
+++ src/Rok_3_21/ZD2/Oval.java
@@ -0,0 +1,33 @@
+package Rok_3_21.ZD2;
+
+public class Oval extends Shape{
+
+    public Oval() {
+        super();
+    }
+
+    @Override
+    protected void drawShape() {
+        System.out.println("Drawing a " + getClass().getSimpleName());
+    }
+
+    @Override
+    protected void playShapeSound() {
+        System.out.println("Playing a sound specific for - " + getClass().getSimpleName());
+    }
+
+    @Override
+    protected void eraseShape() {
+        System.out.println("Erasing a " + getClass().getSimpleName());
+    }
+
+    @Override
+    protected void translateShape() {
+        System.out.println("Translating a: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
